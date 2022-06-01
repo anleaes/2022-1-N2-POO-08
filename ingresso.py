129b5bc083181082f4cd768569fe627408d7723e
GabrielCCruz <costadacruz@outlook.com>
2022-05-31 21:43:03 -0300

    criacao da classe ingresso

---
+++ ingresso.py
@@ -0,0 +1,5 @@
+class Ingresso:
+    def _init_(self, id_ingresso, data, pessoaclientevisitante):
+        self._id_ingresso = id_ingresso
+        self._data = data
+        self._pessoaclientevisitante = pessoaclientevisitante
